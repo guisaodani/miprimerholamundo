fbbdf7e388b452a59cc1c971860d9835fcbb5564
JUAN DANIEL GUISAO CASTRO <juanguisao246051@correo.itm.edu.co>
2024-02-10 22:18:52 -0500

    se actualiza

--- miprimerholamundo/Program.cs
+++ miprimerholamundo/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola, Perros!. como estan? espero esten muy bien :D");
+Console.WriteLine("Hola, Perros!. como estan? espero esten muy bien :D jeje");
