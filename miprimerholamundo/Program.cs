86f68ddf171ede0508663dfea3e5764800bb6297
JUAN DANIEL GUISAO CASTRO <juanguisao246051@correo.itm.edu.co>
2024-02-10 22:15:03 -0500

    se agrega texto "espero esten muy bien"

--- miprimerholamundo/Program.cs
+++ miprimerholamundo/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola, Perros!. como estan?");
+Console.WriteLine("Hola, Perros!. como estan? espero esten muy bien");
